f9df0d0f48c34ecd8bc667fdfaeb79ca9c631b78
TIMM SNOW <two.tone.snow@gmail.com>
2023-09-04 12:00:08 -1000

    adjust column

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Card, Carousel, Container } from "react-bootstrap";
+import { Button, Card, Carousel, Container, Col, Row } from "react-bootstrap";
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import InfoSection from './InfoSection.jsx';
 import '../Info.css';
@@ -38,34 +38,38 @@ const Info = (props) => {
     };
 
   return (
-    <>
-    <Container className="carousel-container">
-      <Carousel interval={null} slide={false} activeIndex={index} onSelect={handleSelect}>
-        {dataKeyArray.map(section => {
-              return(
-                  <Carousel.Item key={"carousel-" + section}>
-                    <Card className="no-border">
-                      <div className="carousel-controls">
-                        <Button variant="outline-warning" onClick={handlePrev}>
-                          <BsChevronLeft />
-                        </Button>
-                        <h2>
-                          {data[section]['header']}
-                        </h2>
-                        <Button variant="outline-warning" onClick={handleNext}>
-                          <BsChevronRight />
-                        </Button>
-                      </div>
-                      <div className="p-5">
-                        <InfoSection sectionNumber={section} sectionData={data[section]}/>
-                      </div>
-                    </Card>
-                  </Carousel.Item>
-                )
-            })}
-        </Carousel>
-    </Container>
-    </>
+    <Row>
+      <Col xs={2}>
+      </Col>
+      <Col xs={8} className="carousel-container">
+        <Carousel interval={null} slide={false} activeIndex={index} onSelect={handleSelect}>
+          {dataKeyArray.map(section => {
+                return(
+                    <Carousel.Item key={"carousel-" + section}>
+                      <Card className="no-border">
+                        <div className="carousel-controls">
+                          <Button variant="outline-warning" onClick={handlePrev}>
+                            <BsChevronLeft />
+                          </Button>
+                          <h2>
+                            {data[section]['header']}
+                          </h2>
+                          <Button variant="outline-warning" onClick={handleNext}>
+                            <BsChevronRight />
+                          </Button>
+                        </div>
+                        <div className="p-5">
+                          <InfoSection sectionNumber={section} sectionData={data[section]}/>
+                        </div>
+                      </Card>
+                    </Carousel.Item>
+                  )
+              })}
+          </Carousel>
+      </Col>
+      <Col xs={2}>
+      </Col>
+    </Row>
   );
 }
   return (
