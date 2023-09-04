67f4e48f9014cef891ff4c62135e370806f72f84
TIMM SNOW <two.tone.snow@gmail.com>
2023-09-04 07:23:35 -1000

    updates dependencies

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import '@babel/polyfill';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
